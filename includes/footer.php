3a671f022f2189a8567a4fdda9ee1a2f4503ce94
Andrew Mason <andrewmasonmedia@gmail.com>
2018-11-20 09:22:47 -0500

    First commit

---
+++ includes/footer.php
@@ -0,0 +1,119 @@
+<!--page footer-->
+<footer>
+    <div class="container-fluid bg-orange">
+        <div class="row d-flex justify-content-center align-items-center bg-img-footer min-50 rellax">
+            <img class="jaxx-logo-text rellax" src="/img/Jaxx_Primary_Icon_RGB_Screeen.svg" alt="Jaxx Liberty">
+        </div>
+        <div class="row d-flex d-flex justify-content-center align-items-end text-white bg-orange px-3 pt-5">
+            <div class="col-lg-3 col-md-6 px-4 py-3 skew">
+                <ul class="list-unstyled w-100">
+                    <li class="h4 font-weight-bold border-bottom pb-3">Jaxx Liberty</li>
+                    <li> <a href="/features.html" class="animsition-link" data-animsition-out-class="fade-out-left-sm" data-animsition-out-duration="800">Features</a></li>
+                    <li> <a href="/changeLog.html" class="animsition-link" data-animsition-out-class="fade-out-left-sm" data-animsition-out-duration="800">Changelog</a></li>
+                    <li> <a href="/legal.html" class="animsition-link" data-animsition-out-class="zoom-out-sm" data-animsition-out-duration="800">Terms of Service</a></li>
+                    <li> <a href="/thirdparties-current.html" class="animsition-link" data-animsition-out-class="fade-out-right-sm" data-animsition-out-duration="800">Third-Party Agreements</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 px-4 py-3 skew">
+                <ul class="list-unstyled w-100">
+                    <li class="h4 font-weight-bold border-bottom pb-3">Company</li>
+                    <li> <a href="/about.html" class="animsition-link" data-animsition-out-class="rotate-out-sm" data-animsition-out-duration="800">About</a></li>
+                    <li> <a href="/community.html" class="animsition-link" data-animsition-out-class="fade-out-left-sm" data-animsition-out-duration="800">Community</a></li>
+                    <li> <a href="/careers.html" class="animsition-link" data-animsition-out-class="fade-out-up-lg" data-animsition-out-duration="800">Careers</a></li>
+                    <li> <a href="https://blog.jaxx.io/">Blog</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 px-4 py-3 skew">
+                <ul class="list-unstyled w-100">
+                    <li class="h4 font-weight-bold border-bottom pb-3">Download</li>
+                    <li><a href="https://play.google.com/store/apps/details?id=com.liberty.jaxx" class="animsition-link" data-animsition-out-class="fade-out-up-sm" data-animsition-out-duration="800">Android</a></li>
+                    <li><a href="https://play.google.com/store/apps/details?id=com.liberty.jaxx" target="_blank">Tablet</a></li>
+                    <li><a href="https://chrome.google.com/webstore/detail/jaxx-liberty/cjelfplplebdjjenllpjcblmjkfcffne" target="_blank">Chrome</a></li>
+                    <li><a class="animsition-link" href="/downloads.html" data-animsition-out-class="fade-out-down" data-animsition-out-duration="800">Desktop</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 px-4 py-3 skew">
+                <ul class="list-unstyled w-100">
+                    <li class="h4 font-weight-bold border-bottom pb-3">Connect</li>
+                    <li> <a href="/support.html" class="animsition-link" data-animsition-out-class="fade-out-left-sm" data-animsition-out-duration="800">Support</a></li>
+                    <li> <a href="https://www.facebook.com/JaxxWallet" target="_blank">Facebook</a></li>
+                    <li> <a href="https://twitter.com/jaxx_io" target="_blank">Twitter</a> / <a href="https://twitter.com/Jaxx_Support" target="_blank">Twitter Support</a> </li>
+                    <li> <a href="https://www.reddit.com/r/jaxx/" target="_blank">Reddit</a></li>
+                </ul>
+            </div>
+        </div>
+        <!--end nest container-->
+    </div>
+    <!--end row-->
+    <!-- copyright + branding-->
+    <div class="row d-flex copyright">
+        <div class="col-sm-12 d-flex justify-content-center text-center py-3 pt-4 navy">
+            <p class="font-weight-bold">&copy; Decentral Inc. <img class="footer-leaf mx-1" src="/img/jaxx-maple-leaf-navy-2.png" alt="Decentral inc."> Made with heart in Canada</p>
+        </div>
+    </div>
+</footer>
+<script src="js/vendor/modernizr-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+<!--cookies-->
+<script src="/js/cookieconsent.min.js"></script>
+<!--scrollmagic-->
+<script src="/js/scrollmagic/minified/ScrollMagic.min.js"></script>
+<script src="/js/scrollmagic/minified/plugins/animation.gsap.min.js"></script>
+<!--gsap-->
+<script src="/js/gsap/TweenMax.min.js"></script>
+<script src="/js/gsap/TimelineLite.min.js"></script>
+<script src="/js/gsap/plugins/CSSPlugin.min.js"></script>
+<!--rellax.js-->
+<script src="/js/rellax.min.js"></script>
+<!--animisation-->
+<script src="/js/animsition.min.js"></script>
+<!--bootstrap-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="/js/plugins.js"></script>
+<script src="/js/modernizr.js"></script>
+<script src="/js/dynamicpage.js"></script>
+<script src="/js/main.js"></script>
+<!-- Global site tag (gtag.js) -->
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'UA-85893600-1');
+
+</script>
+<!-- Google Analytics -->
+<script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-85893600-1', 'auto');
+    ga('send', 'pageview');
+
+    function googleAnalyticsTrigger(category, label) {
+        ga('send', {
+            hitType: 'event',
+            eventCategory: category,
+            eventAction: 'download',
+            eventLabel: label
+        });
+    }
+
+</script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-85893600-1"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="<?php
+
+    if(isset($metaD) && !empty($metaD)) { 
+       echo $metaD; 
+    } 
+    else { 
+       echo "Some meta description"; 
+    } ?>" />
+
+    <title><?php 
+    if(isset($title) && !empty($title)) { 
+       echo $title; 
+    } 
+    else { 
+       echo "Default title tag"; 
+    } ?></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="jms-touch-icon-1.png">
+    <link rel="short icon" href="jms.ico">
+    <!-- Place favicon.ico in the root directory -->
+     <!--core meta data-->
+    <meta name="description" content="Workshops in Wild Places is an international art teaching initiative developed by Canadian artist and educator Janice Mason Steeves.">
+    <meta name="keywords" content="Janice Mason Steeves,Art,Abstract,Workshops in Wild Places">
+    <!-- Open Graph Meta Data -->
+    <meta property="og:description" content="">
+    <meta property="og:image" content="https://workshopsinwildplaces.com/img/jms-wiwp-social-banner.png">
+    <meta property="og:site_name" content="workshopsinwildplaces.com">
+    <meta property="og:title" content="Workshops in Wild Places">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://workshopsinwildplaces.com">
+    <!-- Twitter Card Meta Data -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="https://workshopsinwildplaces.com">
+    <meta name="twitter:creator" content="Workshops in Wild Places">
+    <meta name="twitter:title" content="Workshops in Wild Places">
+    <meta name="twitter:description" content="Workshops in Wild Places is an international art teaching initiative developed by Canadian artist and educator Janice Mason Steeves.">
+    <meta name="twitter:image" content="https://workshopsinwildplaces.com/img/jms-wiwp-social-banner.png">
+    <!--google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,500,700,900" rel="stylesheet">
+    <!--animate.css-->
+    <link rel="stylesheet" href="/css/animate.css/animate.min.css">
+    <!--   mailchimp css link -->
+    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+    <!--aos-->
+   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!--font awesome-->
+    <link href="/css/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/main.min.css">
+<!--	original css-->
+	<link rel='stylesheet' type='text/css' href='css/style.css' />
+	
+	<!--[if IE]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+	
+    <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
+    <script type='text/javascript' src='js/modernizr.js'></script>
+    <script type='text/javascript' src='js/dynamicpage.js'></script>
+</head>
+
+<body>
+    <!--nav-->
+        <nav class="navbar fixed-top navbar-expand-lg">
+            <a class="navbar-brand" href="/index.php">
+            <img class="jaxx-navbar-brand" src="/img/JaxxLiberty_Logo_Grouped.svg" alt="Jaxx Liberty">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <img class="jaxx-mobile-menu" src="/img/jaxx-menu-orange.png" alt="Jaxx Liberty Menu">
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/features.php">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/security.php">Security</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/community.php">Community</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://blog.jaxx.io/" target="_blank">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/support.html">Support</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!--end nav-->  
